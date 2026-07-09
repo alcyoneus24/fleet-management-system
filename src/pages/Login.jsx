d22e774887e7f90fc09bb0c484975f6ce559c26f
Alcyoneus <adityataiwade36@gmail.com>
2026-07-09 21:58:47 +0530

    Improve login page UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,20 +8,59 @@ function Login() {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#f4f6f9",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "white",
+          padding: "30px",
+          borderRadius: "10px",
+          boxShadow: "0px 4px 10px rgba(0,0,0,0.1)",
+          width: "350px",
+        }}
+      >
+        <h2 style={{ textAlign: "center" }}>Cloud Fleet Management System</h2>
 
-      <input type="text" placeholder="Username" />
+        <input
+          type="text"
+          placeholder="Username"
+          style={{
+            width: "100%",
+            padding: "10px",
+            marginBottom: "10px",
+            boxSizing: "border-box",
+          }}
+        />
 
-      <br />
-      <br />
+        <input
+          type="password"
+          placeholder="Password"
+          style={{
+            width: "100%",
+            padding: "10px",
+            marginBottom: "15px",
+            boxSizing: "border-box",
+          }}
+        />
 
-      <input type="password" placeholder="Password" />
-
-      <br />
-      <br />
-
-      <button onClick={handleLogin}>Login</button>
+        <button
+          onClick={handleLogin}
+          style={{
+            width: "100%",
+            padding: "10px",
+            cursor: "pointer",
+          }}
+        >
+          Login
+        </button>
+      </div>
     </div>
   );
 }
